74e8fe36ea389910077e7aa54dfabf0a7cd9a0ad
AntoninAt01 <antonintrebosc@etu.unilim.fr>
2024-11-27 10:18:33 +0100

    Dev3

---
+++ src/components/data.js
@@ -0,0 +1,14 @@
+const data = [
+    {
+      title: "Mon projet cool",
+      description: "Une application web innovante pour gérer vos tâches.",
+      image: "/images/project1.png",
+    },
+    {
+      title: "Un autre projet",
+      description: "Une application mobile pour tracker ses habitudes.",
+      image: "/images/project2.png",
+    },
+  ];
+  
+  export default data;
