c1e9e56138758a3b4e9347b5648e0bf3a2cf1d55
AntoninAt01 <antonintrebosc@etu.unilim.fr>
2024-12-10 13:34:04 +0100

    Dev5

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				Primary: '#C5FC59',
+				White: '#FDFDFD',
+				Black: '#000E17',
+				Black2: '#212121',
+			},
+			fontFamily: {
+				Inter: ['Inter', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
 }
