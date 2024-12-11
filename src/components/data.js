ab3eaf2373ae1b763e9ad735c731e45cf73627f1
AntoninAt01 <antonintrebosc@etu.unilim.fr>
2024-12-11 14:04:21 +0100

    Dev6

--- src/components/data.js
+++ src/components/data.js
@@ -1,13 +1,31 @@
 const data = [
     {
-      title: "Mon projet cool",
-      description: "Une application web innovante pour gérer vos tâches.",
-      image: "/images/project1.png",
+      title: "Motion design - Teaser vidéo",
+      sous_titre: "Concerts HIERO de 2025",
+      cover: "/covers/Cover_hiero.png",
+      description: "Ce projet a été réalisé pour un projet universitaire. L’objectif était de créer une courte vidéo pour les réseaux sociaux en respect de la direction artistique du client, dans laquelle devait figurer une animation de personnage (réalisé sur Photoshop) et communiquer sur l’arrivée des prochains concerts de 2025.",
+      video: "/contenu/Hiero.mp4",
     },
     {
-      title: "Un autre projet",
-      description: "Une application mobile pour tracker ses habitudes.",
-      image: "/images/project2.png",
+      title: "Maquette d'application",
+      sous_titre: "Application de réservation de stades de football",
+      cover: "/covers/Cover_Maquette.png",
+      description: "Cette réalisation est une application de réservation de stades de football au design marqué. L'objectif était de créer une application qui se différencie des applications concurrentes sans négliger la simplicité d'utilisation de l'interface. Ce projet d'application propose de réserver un stade en fonction des filtres choisis (lieu, date, horaire, durée), de suivre les actualités sur les événements locaux concernant le foot, de créer un profil avec un score déterminé par l'historique des résultats de matchs, mais également d'ajouter des amis, de réserver un stade en commun, de créer des équipes et d'organiser des tournois.",
+      image: "/contenu/application.png",
+    },
+    {
+      title: "Motion design - Teaser vidéo",
+      sous_titre: "Concours de talent au festival de Coachella",
+      cover: "/covers/Cover_Coachella.png",
+      description: "Cette vidéo de teaser a été réalisée à l'occasion d'un projet universitaire. Le but était de communiquer sur l'événement d'un concours de talent lors du festival de Coachella. Après avoir déterminé la direction artistique, j’ai créé les différentes illustrations sur Illustrator, puis j’ai réalisé ce court teaser vidéo en motion design pour énoncer les modalités afin de participer au concours.",
+      video: "/contenu/coachella.mp4",
+    },
+    {
+      title: "Modélisation 3D",
+      sous_titre: "Personnage fantaisiste",
+      cover: "/covers/Cover_Perso.png",
+      description: "Ce projet est une modélisation 3D d'un personnage fantaisiste que j'ai moi-même imaginé. Il s'agit d'un explorateur de style cartoon au visage froid.",
+      image: "/contenu/persoBlender.png",
     },
   ];
   
